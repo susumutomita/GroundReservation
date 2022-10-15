ff9bcf3e33ffdd6e0160c756b811334727252047
susumu tomita <oyster880@gmail.com>
2022-10-15 17:24:33 +0900

    Initial Commit

---
+++ GroundReservation/GroundReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundReservationApp.swift
+//  GroundReservation
+//
+//  Created by susumu tomita on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct GroundReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
