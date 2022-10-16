ce05d00687954495828e7990cf9617c9303b4f58
susumu tomita <oyster880@gmail.com>
2022-10-17 07:33:06 +0900

    Update

---
+++ GroundReservation/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GroundReservation
+//
+//  Created by susumu tomita on 2022/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
