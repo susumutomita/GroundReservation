1aafa41cf6babe02a4a2c2102acbd228409dbd94
susumu tomita <oyster880@gmail.com>
2022-10-18 07:37:55 +0900

    Update

---
+++ GroundReservation/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  GroundReservation
+//
+//  Created by susumu tomita on 2022/10/18.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
--- GroundReservation/Views/Landmarks/HexagonParameters.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HexagonParameters.swift
-//  GroundReservation
-//
-//  Created by susumu tomita on 2022/10/17.
-//
-
-import Foundation
